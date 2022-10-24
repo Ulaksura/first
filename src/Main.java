ec4b6d052582327b9265df492705ece555fbf62b
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2022-10-24 19:48:15 +0300

    примеры вызова команд в мэйне

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cocktail cocktail=new Cocktail("Milk","Banana");
+        // так получилось, что у нас нет Геттера для name и kkal, а они нам очень нужны
+        // для такого прикола и используют рефлексию
+        ReflectionAPICheck Checker = new ReflectionAPICheck(); // объект нашей рефлексии
 
+        Checker.getFilds(cocktail,"name");
+        Checker.getFilds(cocktail,"kkal");
+        Checker.getMethod(cocktail,"showFruit");
+        Checker.getMethod(cocktail,"setName","милкшейк"); // меняем имя
+        Checker.getFilds(cocktail,"name"); // dsdlbv bvz
+        Checker.showClassName(cocktail);
     }
 }
