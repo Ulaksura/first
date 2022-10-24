4011fe579eff996c2659d8cf7a4a4516238ebd42
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2022-10-24 19:46:48 +0300

    Новые методы get

--- src/Cocktail.java
+++ src/Cocktail.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Cocktail {
+public class Cocktail {
+    private String name;
+    private String drink;
+    private String fruit;
+    private int kkal;
+    Cocktail(String d, String f)
+    {
+        this.name=f+d;
+        this.drink=d;
+        this.fruit=f;
+        this.kkal= (int)(Math.random()*name.length()*23);
+    }
+
+
+    public String getDrink() {
+        return drink;
+    }
+
+    private void showFruit() {
+        System.out.println(fruit);
+    } // приватный для примера
+    private void setName(String n)
+    {
+        this.name = n;
+    } // так же приватный для примера
 }
--- src/ReflectionAPICheck.java
+++ src/ReflectionAPICheck.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class ReflectionAPICheck {
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionAPICheck {
+    public void showClassName(Object obj)
+    {
+        Class cl = obj.getClass();
+        System.out.println(cl.getName());
+    }
+    public void showClassFields(Object obj)
+    {
+        Class cl = obj.getClass();
+        // cl.getField() возвращает, если мы знаем конкретное поле
+        // cl.getFields() возвращает массив полей, но ток публичных
+        // нам интересны приватные поля, для этого мы имеем
+        Field[] fields =  cl.getDeclaredFields(); // мы создаем массив типа Field это что-то типа класса полей
+        for(Field i: fields)
+        {
+            System.out.println(i.getName());
+        }
+    }
+    public void showClassMethod(Object obj)
+    {
+        Class cl = obj.getClass();
+        Method[] method= cl.getDeclaredMethods();
+        for(Method i: method)
+        {
+            System.out.println(i.getName());
+        }
+    }
+    public  void getFilds(Object obj, String FieldsName)
+    {
+        try {
+            Field field = obj.getClass().getDeclaredField(FieldsName);
+            field.setAccessible(true);
+            System.out.println (field.get(obj));
+        } catch (NoSuchFieldException | IllegalAccessException e) { // без этого оброботчика ошибок гет не хочет работать
+            e.printStackTrace();
+        }
+    }
+    public  void getMethod(Object obj, String MethodName)
+    {
+        try {
+            Method method = obj.getClass().getDeclaredMethod(MethodName);
+            method.setAccessible(true);
+            method.invoke(obj);
+        }  catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    public static<T> void getMethod(Object obj, String MethodName, T args)
+    {
+        try {
+            Method method = obj.getClass().getDeclaredMethod(MethodName, args.getClass());
+            method.setAccessible(true);
+            method.invoke(obj, args);
+        }  catch ( NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
